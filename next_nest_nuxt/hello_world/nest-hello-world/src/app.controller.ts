5f129be798908356c8804f9ebbb1037caae6f0fe
pbobinsk <pbobinsk@gmail.com>
2025-05-19 16:49:34 +0200

    nest hello i vercel

--- next_nest_nuxt/hello_world/nest-hello-world/src/app.controller.ts
+++ next_nest_nuxt/hello_world/nest-hello-world/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller() // Domyślnie brak prefixu, lub np. @Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello') // Definiuje endpoint GET /hello
   getHello(): string {
     return this.appService.getHello();
   }
