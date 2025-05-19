e1083ead382ab99c762a02dd616498703ed425eb
pbobinsk <pbobinsk@gmail.com>
2025-05-19 18:14:51 +0200

    nuxt complex

---
+++ next_nest_nuxt/complex/nuxt-complex-app/pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Head>
+      <Title>O Nas - Nuxt Demo</Title>
+    </Head>
+    <h1>O Nas</h1>
+    <p>Jesteśmy firmą demonstracyjną pokazującą możliwości Nuxt 3.</p>
+    <p>Naszym celem jest tworzenie świetnych aplikacji webowych z Vue.</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
