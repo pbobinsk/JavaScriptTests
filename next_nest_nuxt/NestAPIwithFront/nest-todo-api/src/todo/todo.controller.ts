350af02a6c0d6d45a13290bb38d687e4fb19ac2b
pbobinsk <pbobinsk@gmail.com>
2025-05-19 18:58:33 +0200

    nest todo i lekki front

---
+++ next_nest_nuxt/NestAPIwithFront/nest-todo-api/src/todo/todo.controller.ts
@@ -0,0 +1,84 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Query, Logger, NotFoundException } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { Todo } from './todo.interface';
+
+@Controller('todo')
+export class TodoController {
+  private readonly logger = new Logger(TodoController.name);
+
+  constructor(private readonly todoService: TodoService) {}
+
+  @Post()
+  create(@Body() todo: Todo): void {
+    this.logger.log('Handling create() request...');
+    return this.todoService.create(todo);
+  }
+
+  @Get('create')
+  createGet(@Query() params: any): void {
+    this.logger.log('Handling create() blueprint request...');
+    const todo = {
+      id: params.id,
+      label: params.label,
+      complete: params.complete,
+    };
+    return this.todoService.create(todo);
+  }
+
+  @Get('mock')
+  createMock(): string {
+    this.logger.log('Handling createMock() blueprint request...');
+    const todos = [
+      {
+        id: 1,
+        label: 'Task numer 1',
+        complete: false,
+      },
+      {
+        id: 2,
+        label: 'Task numer 2',
+        complete: true,
+      },
+      {
+        id: 3,  
+        label: 'Task numer 3',
+        complete: false,
+      },
+    ];
+    this.logger.log(todos);
+    return this.todoService.createAll(todos);
+  }
+
+  @Get()
+  findAll(): Todo[] {
+    this.logger.log('Handling findAll() request...');
+    return this.todoService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): Todo {
+    this.logger.log('Handling findOne() request with id=' + id + '...');
+    const todo = this.todoService.findOne(id); // todo jest teraz typu Todo | undefined
+
+    if (!todo) {
+      this.logger.warn(`Todo with id=${id} not found.`);
+      throw new NotFoundException(`Todo with id ${id} not found`); // NestJS automatycznie zamieni to na odpowiedź HTTP 404
+    }
+
+    this.logger.log(`Found todo: ${JSON.stringify(todo)}`);
+    return todo;
+  }
+    
+
+  @Put(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() todo: Todo): void {
+    this.logger.log('Handling update() request with id=' + id + '...');
+    return this.todoService.update(id, todo);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number): void {
+    this.logger.log('Handling remove() request with id=' + id + '...');
+    return this.todoService.remove(id);
+  }
+}
---
+++ next_nest_nuxt/NestAPIwithFront/nest-todo-api/src/todo/todo.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { Todo } from './todo.interface';
+
+@Injectable()
+export class TodoService {
+  private storage: Todo[] = [];
+
+  create(todo: Todo): void {
+    const currentMaxId = Math.max(...this.storage.map((t: Todo) => t.id));
+    todo.id = currentMaxId + 1;
+    this.storage.push(todo);
+  }
+  createAll(todos: Todo[]): string {
+    this.storage = todos;
+    return "OK"
+  }
+
+  findAll(): Todo[] {
+    return this.storage;
+  }
+
+  findOne(id: number): Todo | undefined{
+    return this.storage.find((t: Todo) => t.id === id);
+  }
+
+  update(id: number, todo: Todo): void {
+    const index = this.storage.findIndex((t: Todo) => t.id === id);
+    this.storage[index] = todo;
+    }
+
+  remove(id: number): void {
+    const index = this.storage.findIndex((t: Todo) => t.id === id);
+    this.storage.splice(index, 1);
+  }
+
+}
